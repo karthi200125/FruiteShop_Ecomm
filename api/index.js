6737ed78166a26013c5af86828068542af33b0f0
Karthi Keyan <125793976+karthi200125@users.noreply.github.com>
2023-10-31 01:56:48 -0700

    Update url

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.use('/user', UserRouter);
 app.use('/product', ProductRouter);
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', 'https://fruite-shop-xh11.vercel.app/');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
