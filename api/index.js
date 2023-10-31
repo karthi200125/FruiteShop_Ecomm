13f1617f5d9173a69c02c242fe16adce8149c4a2
Karthi Keyan <125793976+karthi200125@users.noreply.github.com>
2023-10-31 01:56:10 -0700

    Update url

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://fruite-shop-xh11.vercel.app/',
     credentials: true,
   })
 );
